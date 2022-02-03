675657635d7a1a15a3822199a75b023df7a4084a
DhanaBermudez <98503778+DhanaBermudez@users.noreply.github.com>
2022-02-03 20:13:22 +0800

    Add files via upload

---
+++ Prelim-Demo-1/about.js
@@ -0,0 +1,6 @@
+module.exports = function(userName) {
+  var w1 = 'Hello John Smith';
+  var w2 = 'This activity will teach on how to deal with a simple server and local modules in Node.js'
+  return w1 + ' ' + userName + '. ' + w2;
+}
+
---
+++ Prelim-Demo-1/contact.js
@@ -0,0 +1,6 @@
+module.exports = function(userName) {
+  var w1 = 'John Smith, ';
+  var w2 = 'if you want additional details about this activity go to this site: https://www.tutorialsteacher.com/nodejs/nodejs-tutorials'
+  return userName + ', ' + w2;
+}
+
